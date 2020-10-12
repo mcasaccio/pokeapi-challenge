d6086136268b5af27cdddf1a615df97207b575f1
Matías Casaccio <7736740+mcasaccio@users.noreply.github.com>
2020-10-12 17:58:45 -0300

    Limite agregado: 5

--- src/helpers/fetchPokemons.js
+++ src/helpers/fetchPokemons.js
@@ -2,7 +2,7 @@ const baseUrl = 'https://pokeapi.co/api/v2' //URL API en duro
 
 export const fetchPokemons = (endpoint, data, method = 'GET') => {
 
-    const url = `${baseUrl}/${endpoint}`;
+    const url = `${baseUrl}/${endpoint}?limit=5`;
 
     if (method === 'GET') {
         return fetch(url);
