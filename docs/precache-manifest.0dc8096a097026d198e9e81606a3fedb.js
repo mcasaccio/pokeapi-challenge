14c263c13aaca1bdbc50316de1d630c7e6d5adcd
Nomak <mati.casaccio@gmail.com>
2020-10-12 17:37:17 -0300

    add homepage

---
+++ docs/precache-manifest.0dc8096a097026d198e9e81606a3fedb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f020ebd4b9a8b756a2edd6efb2ee3d",
+    "url": "/pokeapi-challenge/index.html"
+  },
+  {
+    "revision": "f9e11cb3ee3a6eac7631",
+    "url": "/pokeapi-challenge/static/js/2.bee91552.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/pokeapi-challenge/static/js/2.bee91552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eee2237254c4e476dd2",
+    "url": "/pokeapi-challenge/static/js/main.4e0e48c1.chunk.js"
+  },
+  {
+    "revision": "2e3e2c491c761449080b",
+    "url": "/pokeapi-challenge/static/js/runtime-main.f7f791b3.js"
+  },
+  {
+    "revision": "4bd7d03523587a936afb4830bfc18ec5",
+    "url": "/pokeapi-challenge/static/media/spain.4bd7d035.svg"
+  },
+  {
+    "revision": "4a90ee18671e922d4eb77cd821ed821b",
+    "url": "/pokeapi-challenge/static/media/unitedStates.4a90ee18.svg"
+  }
+]);
